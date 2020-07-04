08ed559cb258f670ab1e1edeae1ba5919824b678
01-hardik <01-hardik@users.noreply.github.com>
2020-07-05 04:43:45 +0530

    Updates

---
+++ precache-manifest.fec23a6ca7f20f7ec5cc5080f31847ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd73a0015de091fdd6589272c1ef4818",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "11a3f5f254150842f150",
+    "url": "/To-Do-App-in-React/static/css/main.f171ecbb.chunk.css"
+  },
+  {
+    "revision": "351b98a1b00696f0f573",
+    "url": "/To-Do-App-in-React/static/js/2.c42a95b3.chunk.js"
+  },
+  {
+    "revision": "11a3f5f254150842f150",
+    "url": "/To-Do-App-in-React/static/js/main.6c985610.chunk.js"
+  },
+  {
+    "revision": "759cf7b4cf17cf63d488",
+    "url": "/To-Do-App-in-React/static/js/runtime~main.60884708.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App-in-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
